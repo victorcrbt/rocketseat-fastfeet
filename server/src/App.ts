762178fe6a6a357a75a603b5e6cd2a4d1d58702e
Victor Batalha <madivcb@gmail.com>
2020-02-15 03:36:45 -0300

    server: basic server setup

---
+++ server/src/App.ts
@@ -0,0 +1,26 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+
+class App {
+  public server: Application;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+
+  private routes(): void {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ server/src/server.ts
@@ -0,0 +1,3 @@
+import server from './App';
+
+server.listen(3333, () => console.log('Server running on port 3333...'));
