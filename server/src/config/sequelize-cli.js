1117fe7d41a8b9392c784050176aba68ba39fff4
Victor Batalha <madivcb@gmail.com>
2020-02-15 14:23:37 -0300

    server: separate database config from cli config
    Because the TypeScript and ES6 syntax was conflicting in the sequelize-cli, I separated in two files, one for the database and another for the CLI, at least for now.

--- server/src/config/sequelize-cli.js
+++ server/src/config/sequelize-cli.js
@@ -1,13 +1,11 @@
-import('dotenv/config');
+require('dotenv/config');
 
-const dialect = process.env.DB_DIALECT as 'postgres' | 'sqlite' | 'mysql';
-
-export default {
+module.exports = {
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  dialect,
+  dialect: process.env.DB_DIALECT,
   storage: './__test__/database.sqlite',
   define: {
     timestamps: true,
