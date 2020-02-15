ed3c8faff91ce2b71f952197a9e0558c9d4d80b9
Victor Batalha <madivcb@gmail.com>
2020-02-15 13:40:20 -0300

    server: database config file

---
+++ server/src/config/database.ts
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+module.exports = {
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dialect: process.env.DB_DIALECT,
+  storage: './__test__/database.sqlite',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
