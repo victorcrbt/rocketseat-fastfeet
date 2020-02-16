e14228261ea423154b726c502fc8a61ec565f984
Victor Batalha <madivcb@gmail.com>
2020-02-16 00:26:14 -0300

    server: created hook to hash password

--- server/src/app/models/User.ts
+++ server/src/app/models/User.ts
@@ -1,9 +1,11 @@
 import Sequelize, { Sequelize as Connection } from 'sequelize';
 import { Model } from 'sequelize-typescript';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   public readonly id: number;
   public email: string;
+  public password: string;
   public password_hash: string;
   public readonly created_at: Date;
   public readonly updated_at: Date;
@@ -12,10 +14,17 @@ class User extends Model {
     super.init(
       {
         email: Sequelize.INTEGER,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.INTEGER,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async (user: User) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10);
+      }
+    });
 
     return this;
   }
