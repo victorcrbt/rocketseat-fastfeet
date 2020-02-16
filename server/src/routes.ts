cf7e17964b85e911b0ad3fc977c58e28a07aae8a
Victor Batalha <madivcb@gmail.com>
2020-02-16 01:39:17 -0300

    server: add route auth protection

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -7,6 +9,7 @@ const routes = Router();
 
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
 routes.post('/users', UserController.store);
 
 export default routes;
