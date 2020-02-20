3a7b737fb170b4d952d41c6cbc588b37fdf5b72d
Victor Batalha <madivcb@gmail.com>
2020-02-19 22:57:53 -0300

    server: add unique email validation on user store

--- server/src/app/validators/userStore.ts
+++ server/src/app/validators/userStore.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import * as yup from 'yup';
 
+import User from '../models/User';
+
 interface Schema {
   name: string;
   email: string;
@@ -14,8 +16,20 @@ export default async (
 ): Promise<void | Response> => {
   const schema: yup.Schema<Schema> = yup.object().shape({
     name: yup.string().required('O campo nome é obrigatório.'),
-    email: yup.string().required('O campo e-mail é obrigatório.'),
-    password: yup.string().required('O campo senha é obrigatório.'),
+    email: yup
+      .string()
+      .required('O campo e-mail é obrigatório.')
+      .test('unique', 'O e-mail informado já está em uso.', async email => {
+        const userExists = await User.findOne({ where: { email } });
+
+        if (!userExists) return true;
+
+        return false;
+      }),
+    password: yup
+      .string()
+      .required('O campo senha é obrigatório.')
+      .min(6, 'A senha deve conter ao menos 6 caractéres.'),
   });
 
   try {
