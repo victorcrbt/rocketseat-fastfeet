d203db21bfe1547a58f6c4946b22895ca39fd849
Victor Batalha <madivcb@gmail.com>
2020-02-20 16:53:34 -0300

    server: fixed validation if the value is undefined

--- server/src/utils/yupMethods.ts
+++ server/src/utils/yupMethods.ts
@@ -2,11 +2,13 @@
 import * as yup from 'yup';
 
 export default (): any => ({
-  exists: yup.addMethod(yup.string, 'existsWithID', function(
+  existsWithID: yup.addMethod(yup.string, 'existsWithID', function(
     model: any,
     msg: string
   ): any {
     return this.test('exists', msg, async value => {
+      if (!value) return false;
+
       const exists = await model.findByPk(value);
 
       if (exists) return true;
