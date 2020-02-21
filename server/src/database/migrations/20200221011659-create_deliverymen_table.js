345b4cb34002e3964b759fdd89c5cd5173ec5dd5
Victor Batalha <madivcb@gmail.com>
2020-02-20 22:26:33 -0300

    server: created migration for files and delivermen table

---
+++ server/src/database/migrations/20200221011659-create_deliverymen_table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('deliverymen', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avatar_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: queryInterface => queryInterface.dropTable('deliverymen'),
+};
