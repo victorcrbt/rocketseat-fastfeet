0659c78ccd3efba0c8e5bf5e5a15219a2e2d96fe
Victor Batalha <madivcb@gmail.com>
2020-02-25 15:46:09 -0300

    server: mail templates setup

--- server/src/lib/Mail.ts
+++ server/src/lib/Mail.ts
@@ -1,6 +1,16 @@
 import { createTransport, Transporter, SendMailOptions } from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
 
 import mailConfig from '../config/nodemailer';
+
+type MailOptions = SendMailOptions & {
+  template: string;
+  context: {
+    [key: string]: string | number | boolean;
+  };
+};
 
 class Mail {
   public transporter: Transporter;
@@ -15,9 +25,29 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
   }
 
-  public sendMail(msg: SendMailOptions): Promise<any> {
+  public configureTemplates(): void {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
+  }
+
+  public sendMail(msg: MailOptions): Promise<any> {
     return this.transporter.sendMail({
       ...mailConfig.default,
       ...msg,
