4f509d5807793de9ba72e12cc785f680f50b3b67
Victor Batalha <madivcb@gmail.com>
2020-02-26 23:54:03 -0300

    client: defined dark and light themes

---
+++ client/src/styles/themes/global.ts
@@ -0,0 +1,12 @@
+export default {
+  font: {
+    family: "'Roboto', sans-serif",
+  },
+
+  colors: {
+    success: '#dff0df',
+    error: '#fab0b0',
+    warn: '#f0f0df',
+    info: '#bad2ff',
+  },
+};
