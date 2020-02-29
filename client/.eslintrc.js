84c3c3e5a7c452742b53cec047f7cc19e5dfe08e
Victor Batalha <madivcb@gmail.com>
2020-02-29 11:50:24 -0300

    client: disabled typescript rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/button-has-type': 'off',
     'no-param-reassign': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
     'import/parsers': {
