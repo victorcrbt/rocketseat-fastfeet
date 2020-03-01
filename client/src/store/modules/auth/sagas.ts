b0144e50a832896de377c42886180c3aee758113
Victor Batalha <madivcb@gmail.com>
2020-03-01 18:46:53 -0300

    client: created sagas to handle signout and set token to headers

--- client/src/store/modules/auth/sagas.ts
+++ client/src/store/modules/auth/sagas.ts
@@ -1,6 +1,7 @@
 import { call, put, takeLatest, all } from 'redux-saga/effects';
 
 import api from '~/services/api';
+import history from '~/services/history';
 
 import { AuthTypes } from './types';
 import { signInSuccess, signInFailure } from './actions';
@@ -21,10 +22,30 @@ export function* signIn({ payload }: SignInProps) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
   } catch (error) {
     yield put(signInFailure());
   }
 }
 
-export default all([takeLatest<SignInProps>(AuthTypes.SIGNIN_REQUEST, signIn)]);
+export function signOut() {
+  history.push('/');
+}
+
+export function setToken({ payload }: any) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest<SignInProps>(AuthTypes.SIGNIN_REQUEST, signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+  takeLatest('persist/REHYDRATE', setToken),
+]);
