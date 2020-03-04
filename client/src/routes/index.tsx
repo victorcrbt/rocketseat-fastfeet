87df162760eca2f766746faffa4ec00c7461e3bf
Victor Batalha <madivcb@gmail.com>
2020-03-04 14:50:17 -0300

    client: changed base url redirect

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Route from './Route';
 
-import Encomendas from '~/pages/Encomendas';
+import Deliveries from '~/pages/Deliveries';
 import SignIn from '~/pages/SignIn';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/encomendas" component={Encomendas} isPrivate />
-
       <Route path="/login" component={SignIn} />
-      <Redirect from="/" to="/encomendas" />
+
+      <Route path="/encomendas" component={Deliveries} isPrivate />
+
+      <Route exact path="/" component={Deliveries} />
     </Switch>
   );
 };
